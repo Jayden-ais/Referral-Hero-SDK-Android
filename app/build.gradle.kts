2c470ca1b2ac0c642f1b99596c6267123929efe9
jayden <jayden@aistechnolabs.org>
2023-06-14 13:23:13 +0530

    0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.github.Jayden-ais:ReferralHero:13.6.27")
+   // implementation("com.github.Jayden-ais:Referral-Hero-SDK-Android:0.0.0")
 }
